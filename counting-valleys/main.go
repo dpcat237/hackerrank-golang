c65732ac6f75bb629f09c8b09506119a7c8c467a
Denys Pasishnyi <dpcat237@gmail.com>
2019-11-30 13:58:16 +0200

    Add counting-valleys

---
+++ counting-valleys/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the countingValleys function below.
+func countingValleys(n int32, s string) int32 {
+	c := 0
+	if len(s) == 0 {
+		return int32(c)
+	}
+
+	isNewValley := false
+	level := 0
+	for _, l := range s {
+		if l == 68 {
+			if level == 0 {
+				isNewValley = true
+			}
+			level--
+		} else {
+			level++
+		}
+
+		if isNewValley {
+			c++
+			isNewValley = false
+		}
+	}
+	return int32(c)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	s := readLine(reader)
+
+	result := countingValleys(n, s)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
